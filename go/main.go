5fda94e9b01baed232e4c2accf832ab36b9235d8
Michael Lange <dingoeatingfuzz@gmail.com>
2024-03-20 23:41:03 -0700

    Should we just rewrite in go?

---
+++ go/main.go
@@ -0,0 +1,26 @@
+package main
+
+// TODO
+// 1. Port the git.ts file to golang
+// 2. Port vcs/github to golang
+// 3. Devise a way to run scripts in parallel (fan out? semaphor?)
+// 4. Use bubbletea to make things pretty. They will probably look something like:
+//
+// Git
+//
+// Commits  |||||||||||||||||-------------- 50%
+// Authors  |||||||||||||||||||||||||||---- 80%
+// Tags     ------------------------------- 0%
+// Branches ------------------------------- 0%
+//
+// GitHub
+//
+// Issues      |||||||--------------------- 10%
+// PRs         ||-------------------------- 3%
+// Discussions ||||------------------------ 7%
+//
+// 5. Interactive script selectors
+// 6. Other VCS APIs
+
+func main() {
+}
